f9de57ced7af15320be354ac5d30fe1cd5b2d746
manibrar <manvirbrar1988@gmail.com>
2018-06-13 21:11:13 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// Sparta Javascript Prompt Calculator
+
+// Added a prompt to ask the user to choose between advanced and basic calculator
+var whichCalculator = prompt("Which calculator would you like to use?  (a) for advanced and (b) for basic ");
+
+
+
+//Basic calculator (B)
+
+//Add
+//multiply
+//divide
+//subtract
+
+// Advanced Calculator (A)
+//Modulus
+//Power
+//Sqr root
+//BMI
